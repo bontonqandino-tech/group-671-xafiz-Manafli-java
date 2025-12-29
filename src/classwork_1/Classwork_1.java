e421ac37b631179e65961b54830449aa9bbf3d88
enigma <bontonqandino@gmail.com>
2025-12-29 21:33:53 +0400

    classwork_1.

--- src/classwork_1/Classwork_1.java
+++ src/classwork_1/Classwork_1.java
@@ -26,7 +26,7 @@ public class Classwork_1 {
 //		byte a3 = 5;
 //			 
 //		if (a3 == 5) {
-//			System.out.println("\"Отлично\"");
+//			System.out.println("Отлично");
 //		} else if (a3 ==4) {
 //			System.out.println("Хорошо");
 //		}
@@ -109,11 +109,394 @@ public class Classwork_1 {
 //			 System.out.println(  "Код должен быть трёхзначным"); 
 //		 } 
 //8----------------------------------------------------------
-		byte a8 =5;
 
+
+//byte a8 = 23;
+//	if (a8==11) {
+//		 System.out.println("Утро"); 	
+//		
+//		
 //	}
+//		
+//	if (a8==17) {
+//		 System.out.println("День"); 	
+//		
+//		
+//	}	
+//		
+//	if (a8==22) {
+//		 System.out.println("Вечер"); 	
+//		
+//	}	
+//
+//	if (a8==23) {
+//		 System.out.println("Ночь"); 	
+//		
+//	}	
+	
+//9------------------------------------------------------------
+//	    int a9 =5;
+//		switch(a9) {
+//		case 10-> System.out.println("Зима");
+//		case 5-> System.out.println("Весна");
+//		case 100-> System.out.println("Лето");
+//		case 30-> System.out.println("Осень");
 //	
-//}
-	
+//		
+//	}		
+		
+//10----------------------------------------------------------------		
+//		byte a10 = 23;
+//		if (a10==22) {
+//			 System.out.println("Чётное"); 	
+//			
+//			
+//		}
+//			
+//		if (a10==23) {
+//			 System.out.println("Нечётное"); 	
+//			
+//			
+//		}	
+			
+	//11------------------------------------------------------------------		
+//		char a10 = 'M'; 
+//		if  (  a10=='M') {
+//			 System.out.println("mugic"); 	
+//			
+//			
+//		}
+//			
+//		if  (a10=='Ж') {
+//			 System.out.println("censina"); 	
+//			
+//			
+//		}	
+			
+	//12-----------------------------------------------------------------	
+//		int a12 =100;
+//		if (a12==100) {
+//		 System.out.println("5"); 	
+//		
+//		
+//	}
+//		
+//	if (a12==70) {
+//		 System.out.println("4"); 	
+//		
+//		
+//	}	
+//		
+//	if (a12==50) {
+//		 System.out.println("3"); 	
+//		
+//	}	
+//
+//	if (a12==20) {
+//		 System.out.println("2"); 	
+//		
+//	}	
+		
+		
+//13-----------------------------------------------------
+//	    int a9 =5;
+//		switch(a9) {
+//		case 10-> System.out.println("net");
+//		case 5-> System.out.println("da");
+//		case 100-> System.out.println("net");
 
+//	
+//		
+//	}	
+//	
+		
+		
+		
+		
+		
+		
+		
+		
+	//14--------------------------------------	
+//		byte a10 = 7;
+//		if (a10==7) {
+//			 System.out.println("Детский сад"); 	
+//			
+//			
+//		}
+//			
+//		if (a10==23) {
+//			 System.out.println("Школа"); 	
+//			
+//			
+//		}
+//		
+//		else {
+//			System.out.println("Взрослый");
+//		}
+//		
+		
+		//15-----------------------------------------
+//	    int a9 =3;
+//		switch(a9) {
+//		case 1-> System.out.println("1год");
+//		case 2-> System.out.println("2год");
+//		case 3-> System.out.println("3год");
+//		case 4-> System.out.println("4год");
+//		
+//		
+//		}	
+		
+		
+		
+		//16-----------------------------------------------
+//		byte a10 = 150;
+//		if (a10==150) {
+//			 System.out.println("rost"); 	
+//			
+//			
+//		}
+//			
+//		if (a10==160) {
+//			 System.out.println("ost"); 	
+//			
+//			
+//		}
+//		if (a10==180) {
+//			 System.out.println("ost"); 	
+//			
+//			
+//		}
+
+		
+	//17----------------------------------------------------	
+		//		byte a16 =100;
+//		
+//		if (a16==100) {
+//			 System.out.println("Победитель!"); 	
+//			
+//		
+//		}
+//		
+//		else {
+//			System.out.println("Попробуй ещё");
+//		}
+		
+//	//18------------------------------------------------
+//	    int a6 =100;
+//		switch(a6) {
+//		case 10-> System.out.println("+");
+//		case 50-> System.out.println("-");
+//		case 100-> System.out.println("/");
+//		case 30-> System.out.println("*");
+//	
+		
+//	}
+		
+		
+		
+//19-------------------------------------------------
+//		byte a5 =5;
+//		if (a5 == 5){
+//			System.out.println("vixodnii");
+//		}
+//		else   {
+//			System.out.println("выходной");
+//
+//		}	
+		
+		
+		
+		
+		//20---------------------------------------
+//		byte a5 =5;
+//		if (a5 == 5){
+//			System.out.println("vigral");
+//		}
+//		else   {
+//			System.out.println("net");
+//
+//		}
+		//21-------------------------------------------
+//		byte a5 =5;
+//		if (a5 == 5){
+//			System.out.println("Равны");
+//		}
+//		else   {
+//			System.out.println("Разные");
+//
+//		}
+		//22-----------------------------------------------
+//		byte a10 = 0;
+//		if (a10==0) {
+//			 System.out.println("led"); 	
+//			
+//			
+//		}
+//			
+//		if (a10==99) {
+//			 System.out.println("voda"); 	
+//			
+//			
+//		}
+//		if (a10==100) {
+//			 System.out.println("par"); 	
+//			
+//			
+//		}
+		//23----------------------------------------------------
+//	    int a6 =100;
+//		switch(a6) {
+//		case 10-> System.out.println("");
+//		case 50-> System.out.println("");
+//		case 100-> System.out.println("ошибки сайта");
+//		case 30-> System.out.println("");
+//	
+//		
+//	}
+		//24-----------------------------------------------------
+//		byte a10 = 0;
+//		if (a10==0) {
+//			 System.out.println("Нет питомцев"); 	
+//			
+//			
+//		}
+//			
+//		if (a10==1) {
+//			 System.out.println("Один питомец"); 	
+//			
+//			
+//		}
+//		else  {
+//			 System.out.println("mnogo"); 	
+//			
+//			
+//		}
+		//25---------------------------------------------------------------
+//	    int a6 =5;
+//		switch(a6) {
+//		case 5-> System.out.println("Молодец");
+//		case 4-> System.out.println("Поговорим с родителями");
+//		case 3-> System.out.println("Поговорим с родителями");
+//		case 2-> System.out.println("Поговорим с родителями");
+//		}
+		//26-----------------------------------------------------------------
+//		   int a6 =5;
+//			switch(a6) {
+//			case 1-> System.out.println("a");
+//			case 2-> System.out.println("f");
+//			case 3-> System.out.println("s");
+//			case 4-> System.out.println("s");
+//	case 5-> System.out.println("ddddddddddddddddddddddddddd");
+//	case 6-> System.out.println("s");
+//	case 7-> System.out.println("s");
+//	case 8-> System.out.println("s");
+//			}
+		//27----------------------------------------------------------
+//		byte a10 = 50;
+//		if (a10==50) {
+//			 System.out.println("silniy"); 	
+//			
+//			
+//		}
+//			
+//		if (a10==30) {
+//			 System.out.println("slabiy"); 	
+//			
+//			
+//		}
+		//28-------------------------------------------------------------
+//		byte a28 =4;
+//		if (a28==4) {
+//		 System.out.println("corect"); 		
+//	}
+//		if (a28==3) {
+//			 System.out.println("uncorect"); 		
+//		}
+//29------------------------------------------------------------------------------
+//		byte a10 = 35;
+//		if (a10==35) {
+//			 System.out.println("kid"); 	
+//			
+//			
+//		}
+//			
+//		if (a10==42) {
+//			 System.out.println("vomen"); 	
+//			
+//			
+//		}
+//		if (a10==45) {
+//			 System.out.println("men"); 	
+//			
+//			
+//		}
+//30---------------------------------------------------------------------------
+//	String a30 = "ru";	
+//	if (a30=="ru") {
+//	 System.out.println("privet"); 	
+//	}
 	
+//31-------------------------------------------------------------------------------
+//		byte a10 = 2;
+//		if (a10==2) {
+//			 System.out.println("twice"); 	
+//			
+//			
+//		}
+//			
+//		if (a10==3) {
+//			 System.out.println("little"); 	
+//			
+//			
+//		}
+//		if (a10==5) {
+//			 System.out.println("big"); 	
+//			
+//			
+//		}
+		//32------------------------------------------------------------
+//		byte a10 = 100;
+//		if (a10==20) {
+//			 System.out.println("lou"); 	
+//			
+//			
+//		}
+//			
+//		else {			 System.out.println("ok"); 	
+//}
+		
+		//33-----------------------------------------------------------
+//		byte a10 = 20;
+//		if (a10==20) {
+//			 System.out.println("high"); 	
+//			
+//			
+//		}
+//		else {
+//			 System.out.println("lou"); 	
+//			
+//		}
+		//34------------------------------------------------------------
+//	   int a34 = 5;
+//		switch(a34) {
+//		case 5-> System.out.println("matem");
+//		case 4-> System.out.println("rus");
+//		case 3-> System.out.println("fiz");
+//		
+//		}
+		//35---------------------------------------------------------------
+//		   int a6 =5;
+//			switch(a6) {
+//			case 1-> System.out.println("1");
+//			case 2-> System.out.println("2");
+//			case 3-> System.out.println("3");
+//			case 4-> System.out.println("4");
+//			case 5-> System.out.println("5");
+//			case 6-> System.out.println("6");
+//}	
+//		
+		
+	}		
+
+}
---
+++ src/homework_1/Homework_1.java
@@ -0,0 +1,79 @@
+package homework_1;
+
+public class Homework_1 {
+	public static void main(String[] args) {
+//		for (int i = 0 ; i <= 10; i++) {
+//			System.out.println(i + ": Привет, я учу Java!");
+//			}
+		
+//		
+//				for (int i = 0 ; i <= 20; i++) {
+//			System.out.println(i + "");
+//			}
+//		
+		
+		
+
+		
+//		        
+//		        for(int i = 10; i > 0; i--){
+//		            System.out.println("число : " + i);
+//		        
+//
+//		    }
+		
+	//4?
+		
+		//5
+//		for (;;) {
+//		System.out.println("Я люблю программировать!");
+//		
+//		}
+//		
+		
+		
+		
+		//6
+//		String[] fruits = {"apple", "mango", "banana"};
+//		
+//		System.out.println("2" + fruits[1]);
+//		System.out.println("3"+fruits [2]);
+		
+		//7
+//		String[] fruits = {"яблоко", "банан", "груша", "апельсин", "киви"}; 
+//		System.out.println("Последний фрукт" + fruits[4]);
+//		}
+
+		
+		//8
+		
+//		int[] cake = {1, 0, 1, 0, 1, 1, 1, 1, 0};
+//		for (int i = 0 ; i < cake.length; i++) {
+//		System.out.println("cake " + (cake[i] == 0 ? "not ready": "ready"));
+//		}
+	
+		
+		
+		
+		
+		//9
+//		  int i = 1; 
+//	        do {
+//	            System.out.println(i); 
+//	            i++; 
+//	        } while (i <= 10); 
+		
+		
+		
+		
+		
+//		10
+
+		
+		
+		
+		
+		
+	}	
+	}
+
