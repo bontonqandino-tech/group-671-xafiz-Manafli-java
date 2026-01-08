0e87ab60161d91434ae6deeece4a363eccc457df
enigma <bontonqandino@gmail.com>
2026-01-08 20:52:09 +0400

    Classwork_1

--- src/classwork_1/Classwork_1.java
+++ src/classwork_1/Classwork_1.java
@@ -23,26 +23,26 @@ public class Classwork_1 {
 		
 		// 3 --------------------------------------------------
 			 
-		byte a3 = 5;
-			 
-		if (a3 == 5) {
-			System.out.println("Отлично");
-		} else if (a3 ==4) {
-			System.out.println("Хорошо");
-		}
-	
-		else if (a3 == 3) {
-			System.out.println("Удовлетворительно");
-	}
-	
-		else if (a3 == 2) {
-			System.out.println("Неудовлетворительно");
-	}
-		else {
-		
-			System.out.println("Некорректная оценка");
-	
-	}
+//		byte a3 = 5;
+//			 
+//		if (a3 == 5) {
+//			System.out.println("Отлично");
+//		} else if (a3 ==4) {
+//			System.out.println("Хорошо");
+//		}
+//	
+//		else if (a3 == 3) {
+//			System.out.println("Удовлетворительно");
+//	}
+//	
+//		else if (a3 == 2) {
+//			System.out.println("Неудовлетворительно");
+//	}
+//		else {
+//		
+//			System.out.println("Некорректная оценка");
+//	
+//	}
 		// 4 --------------------------------------------------	
 //		byte a4 =12;
 //		if (a4 % 3 == 0  )
@@ -496,6 +496,35 @@ public class Classwork_1 {
 //}	
 //		
 		
-	}		
+		
+		
+		
+		
+		//1----------------------------------------------
+//1
+//		for(int i = 1; i <= 10; i++){
+//	     System.out.println(" " + i);
+//	}
+		
+//2		
+		
+//		for(int i = 10; i > 0; i--){
+//	     System.out.println("число : " + i);
+//	}	
+		
+//3=--------------------		
+//	    for (int i = 2; i <= 20; i += 2) {
+//            System.out.print(i + " ");
+//        }
+		
+//	}	
+		//4----------------------------------------------------
+	     int sum = 0;
 
+	        for (int i = 1; i <= 50; i++) {
+	            sum = sum + i;
+	        }
+
+	        System.out.println(sum);
+	}
 }
