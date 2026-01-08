d12604c0ca4f1480d4c43753e05370cd142b4733
enigma <bontonqandino@gmail.com>
2026-01-08 20:54:56 +0400

    classwork_2

--- src/classwork_1/Classwork_1.java
+++ src/classwork_1/Classwork_1.java
@@ -500,31 +500,6 @@ public class Classwork_1 {
 		
 		
 		
-		//1----------------------------------------------
-//1
-//		for(int i = 1; i <= 10; i++){
-//	     System.out.println(" " + i);
-//	}
-		
-//2		
-		
-//		for(int i = 10; i > 0; i--){
-//	     System.out.println("число : " + i);
-//	}	
-		
-//3=--------------------		
-//	    for (int i = 2; i <= 20; i += 2) {
-//            System.out.print(i + " ");
-//        }
-		
-//	}	
-		//4----------------------------------------------------
-	     int sum = 0;
-
-	        for (int i = 1; i <= 50; i++) {
-	            sum = sum + i;
-	        }
-
-	        System.out.println(sum);
+	
 	}
 }
---
+++ src/classwork_2/Classwork_2.java
@@ -0,0 +1,37 @@
+package classwork_2;
+
+public class Classwork_2 {
+	public static void main(String[] args) {
+		//1----------------------------------------------
+		//1
+//				for(int i = 1; i <= 10; i++){
+//			     System.out.println(" " + i);
+//			}
+				
+		//2		
+				
+//				for(int i = 10; i > 0; i--){
+//			     System.out.println("число : " + i);
+//			}	
+				
+		//3=--------------------		
+//			    for (int i = 2; i <= 20; i += 2) {
+//		            System.out.print(i + " ");
+//		        }
+				
+//			}	
+				//4----------------------------------------------------
+//			     int sum = 0;
+		//
+//			        for (int i = 1; i <= 50; i++) {
+//			            sum = sum + i;
+//			        }
+		//
+//			        System.out.println(sum);
+				
+				//5----------------------------------
+//			     for (int i = 1; i <= 10; i++) {
+//			            System.out.println("7 * " + i + " = " + (7 * i));
+//			        }
+	}
+}
